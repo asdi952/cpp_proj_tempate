15bb86f828e315429cb41ff5ecf3aea5f0f80fa3
you <ricardo.silvestre.1995@gmail.com>
2021-02-25 16:14:21 +0000

    template

---
+++ create_folder.sh
@@ -0,0 +1,21 @@
+mkdir cpp
+
+
+mkdir header
+
+
+mkdir bin
+
+cd bin
+mkdir normal
+mkdir debug
+
+cd normal
+mkdir preHeader
+mkdir objects
+mkdir depend
+
+cd ../debug
+mkdir preHeader
+mkdir objects
+mkdir depend
